965b9d0345c9f2662a04fb26beb933c401128afe
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2023-06-11 19:50:39 +0700

    Add files via upload

---
+++ JAVA CONCEPT/Eps. 11 - 20/EPS20/src/com/Tutorial/Main.java
@@ -0,0 +1,15 @@
+// Eps.20 If Else Statement
+package com.Tutorial;
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("Nilai = "+ a);
+
+        if (a == 10) {
+            System.out.println("Ini adalah jalur cabang");}
+        else {
+            System.out.println("Ini adalah jalur false");}
+        
+        System.out.println("Program selesai");
+    }    
+}
